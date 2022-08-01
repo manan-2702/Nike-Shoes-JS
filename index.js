4a1e8cf8013c7f2b6984a4aaae2fd10b624a07bd
manan-2702 <mananrajpara2002@gmail.com>
2022-08-01 21:23:26 +0530

    NikeShoesCommit_01

---
+++ index.js
@@ -0,0 +1,5 @@
+let errorparagraph = document.getElementById("error")
+console.log(errorparagraph);
+function purchase() {
+  errorparagraph.textContent = "Something went worng!,Please try again later.";
+}
